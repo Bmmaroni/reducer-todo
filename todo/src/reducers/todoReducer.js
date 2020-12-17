64f118cb522340f6d32233f40b469f25d085aea7
Brandon Maroni <brandon-maroni@lambdastudents.com>
2020-12-16 17:43:53 -0700

    started to build out ADD_TODO in reducer

--- todo/src/reducers/todoReducer.js
+++ todo/src/reducers/todoReducer.js
@@ -1,14 +1,34 @@
-const initialState = {
-  todos: []
+export const ADD_TODO = "ADD_TODO";
+export const TOGGLE_TODO = "TOGGLE_TODO";
+export const CLEAR_COMPLETED = "CLEAR_COMPLETED";
+
+export const initialState = {
+  todos: [
+      {
+        item: 'Learn about reducers',
+        completed: false,
+        id: Date.now()
+    }
+  ]
 };
 
 export const todoReducer = (state, action) => {
   switch(action.type) {
-    case("ADD_TODO"):
+    case ADD_TODO:
+      return {
+        ...state,
+        todos: [
+          ...state.todos,
+          {
+            item: action.payload,
+            completed: false,
+            id: Date.now()
+          }
+        ] 
+      };
+    case TOGGLE_TODO:
       return state;
-    case("TOGGLE_TODO"):
-      return state;
-    case("CLEAR_COMPLETED"):
+    case CLEAR_COMPLETED:
       return state;
     default:
       return state;
