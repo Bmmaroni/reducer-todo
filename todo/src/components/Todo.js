c43ae00bea2574dbde496dfbdbe9abba7660582d
Brandon Maroni <brandon-maroni@lambdastudents.com>
2020-12-16 18:38:23 -0700

    corrected errors to get successful rendering and full functionality

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Todo = (props) => {
   return (
     <div
-      onClick={props.toggleTodo(props.todo.id)}
+      onClick={() => props.toggleTodo(props.todo.id)}
       className={`todo${props.todo.completed ? " completed" : ""}`}
     >
       {props.todo.item}
